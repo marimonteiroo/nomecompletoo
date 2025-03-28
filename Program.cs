bea44620e45e1912dd39364be79db947cfab4268
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-27 21:51:08 -0300

    nomecompleto

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿﻿// Declaracao de variaveis
+string nomeDigitado;
+string sobrenomeDigitado;
+
+
+Console.Write("Precione uma tecla para continuar...");
+Console.ReadKey(true);
+
+Console.Clear();
+
+Console.WriteLine("---Dados Pessoais---");
+
+Console.Write("nome: ");
+nomeDigitado = Console.ReadLine()!;
+
+Console.Write("sobrenome: ");
+sobrenomeDigitado = Console.ReadLine()!;
+
+Console.WriteLine(nomeDigitado);
+Console.WriteLine(sobrenomeDigitado);
+
+Console.WriteLine($"Boa noite, {nomeDigitado} {sobrenomeDigitado}!");
